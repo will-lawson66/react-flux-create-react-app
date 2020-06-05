154726fc5f9c2c455aba8e830a6ece2e47dcef37
Bill Lawson <blawson@firstinspires.org>
2020-06-05 14:31:19 -0400

    7 - Hooks, Component Composition, PropTypes

--- 3-dev-env/src/components/CoursesPage.js
+++ 3-dev-env/src/components/CoursesPage.js
@@ -1,46 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CourseList";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: [],
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  // lifecycle emthod, using arrow function
-  componentDidMount() {
-    getCourses().then((courses) => this.setState({ courses: courses }));
-  }
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => {
-              /* iterate over the courses array */
-              return (
-                <tr key={course.id}>
-                  {" "}
-                  {/* note the key. */}
-                  <td>{course.title}</td>
-                  <td>{course.authorId}</td>
-                  <td>{course.category}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Courses</h2>
+      <CourseList courses={courses} />
+    </>
+  );
 }
 
 export default CoursesPage;
